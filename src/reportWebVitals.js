67da0ba3f013d9459258e99fec2e590c22969571
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-03 17:46:11 +0300

    Fix linters

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,8 +1,8 @@
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({
-       getCLS, getFID, getFCP, getLCP, getTTFB 
-      }) => {
+      getCLS, getFID, getFCP, getLCP, getTTFB,
+    }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
