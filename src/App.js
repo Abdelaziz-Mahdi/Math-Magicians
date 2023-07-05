5f52d682994591a129e3e9f9b2f8055288a46e65
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-05 18:30:46 +0300

    Import the Quate into main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
 
 function App() {
+  const saveClass = 'color-white';
   return (
     <div className="App">
+      <Quote sav={saveClass} />
       <Calculator />
     </div>
   );
