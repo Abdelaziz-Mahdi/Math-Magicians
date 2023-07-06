ab9625e881f740bc08b5ced8cc75152582161071
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-07 02:25:06 +0300

    Create nav and add react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
+import {
+  BrowserRouter, Routes, Route, Outlet,
+} from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
+import Home from './components/Home';
+
+function Nav() {
+  return (
+    <>
+      <a href="/"><h1>Math magicians</h1></a>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/calculator">Calculator</a></li>
+          <li><a href="/quote">Quote</a></li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
   const saveClass = 'color-white';
   return (
-    <div className="App">
-      <Quote sav={saveClass} />
-      <Calculator />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Nav />}>
+          <Route index element={<Home />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<Quote sav={saveClass} />} />
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
